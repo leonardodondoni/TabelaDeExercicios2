593268221aa39acbd6ab6a9fd7739cf2e8c9807a
Leonardo <leonardosiqueira387@gmail.com>
2022-09-04 22:51:57 -0300

    nao fraguei

--- exercicio29.java
+++ exercicio29.java
@@ -1,4 +1,22 @@
 import java.util.Scanner;
 public class exercicio29 {
+    public static void main(String args[]){
+
+        Scanner input = new Scanner(System.in);
+
+        //29. Faça um programa que leia o nome de 3 pessoas e os exiba em ordemcrescente.
+
     
+    System.out.println("Informe o nome de 3 pessoas, o programa exibirá em ordem crescente os nomes.");
+    String nome1 = input.nextLine();
+    String nome2 = input.nextLine();
+    String nome3 = input.nextLine();
+
+    int n1 = nome1.length();
+    int n2 = nome2.length();
+    int n3 = nome3.length();
+
+    int maior,meio,menor;
+
+    }
 }
